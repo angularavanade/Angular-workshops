4c79ea20b3be4b24322998b39373888752d27769
Mateusz Dobrakowski <angularworkshops@gmail.com>
2019-01-21 10:42:59 +0100

    after test workshops

--- board-games/src/app/services/games.service.spec.ts
+++ board-games/src/app/services/games.service.spec.ts
@@ -1,15 +1,38 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GamesService } from './games.service';
+import { HttpClient } from '@angular/common/http';
+import gameMockArray, { HttpClientMock } from '../mocks/HttpClientMock';
 
-describe('GamesService', () => {
+fdescribe('GamesService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [GamesService]
+      providers: [
+        GamesService,
+        {
+          provide: HttpClient, 
+          useClass: HttpClientMock
+        }
+      ]
     });
   });
 
   it('should be created', inject([GamesService], (service: GamesService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get return games', inject([GamesService], (service: GamesService) => {
+    const ob = service.getGames().subscribe( (games) => {
+      expect(games).toEqual(gameMockArray);
+    });
+  }));
+
+  it('should use proper url when calling api', inject([GamesService, HttpClient], (service: GamesService, httpClient: HttpClient) => {
+    const get = spyOn(httpClient,'get');
+    const apiUrl = 'http://webrental.azurewebsites.net/api/boardgames';
+    service.getGames();
+    expect(get).toHaveBeenCalledWith(apiUrl);
+  }));
+
+
 });
